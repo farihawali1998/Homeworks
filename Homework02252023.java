23aa278ad62aa363272b245829ac5222b5f502d2
Fariha Wali <123264982+farihawali1998@users.noreply.github.com>
2023-02-26 14:41:53 -0500

    Add files via upload

---
+++ Homework02252023.java
@@ -0,0 +1,21 @@
+package Homework;
+
+import java.util.LinkedHashSet;
+
+public class Homework02252023 {
+    public static void main(String[] args) {
+        //Create a set of cities in which you want to make sure
+        //that insertion order is maintained. Then remove any city
+        //that starts with "A".
+
+        LinkedHashSet<String> countries=new LinkedHashSet<>();
+        countries.add("Afghanistan");
+        countries.add("Pakistan");
+        countries.add("Azerbijan");
+        countries.add("India");
+
+        countries.removeIf(x-> x.startsWith("A"));
+        System.out.println(countries);
+
+    }
+}
